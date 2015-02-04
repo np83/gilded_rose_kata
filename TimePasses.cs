2d94cb41740bb686b2c9d6f4785dae9088b870e0
np83 <p_wijnhorst@hotmail.com>
2015-02-04 17:51:33 +0100

    Add first draft of tests for validating the current code (admitted not the most beautifull version).

---
+++ TimePasses.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GildedRose.Tests
+{
+	[TestFixture]
+	public class TimePasses : CodedRoseTestBase
+	{
+		public static IEnumerable<int> Days()
+		{
+			for(int i = 0; i <= 20; i++)
+			{
+				yield return i;
+			}
+		}
+
+		[Test]
+		[TestCaseSource("Days")]
+		public void PassTime(int days)
+		{
+			AssertAlwaysConditions();
+			for(int i = 0; i < days; i ++)
+			{
+				NextDay();
+			}
+		}
+	}
+}
--- gilded_rose.cs
+++ gilded_rose.cs
@@ -80,15 +80,20 @@ namespace GildedRose
 			}
 		}
 
-		private static readonly IList<Item> Items = new List<Item>
+		public static IList<Item> CreateDayOne()
 		{
-			new Item { Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20 },
-			new Item { Name = "Aged Brie", SellIn = 2, Quality = 0 },
-			new Item { Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7 },
-			new Item { Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80 },
-			new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 15, Quality = 20 },
-			new Item { Name = "Conjured Mana Cake", SellIn = 3, Quality = 6 },
-		};
+			return new List<Item>
+			{
+				new Item { Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20 },
+				new Item { Name = "Aged Brie", SellIn = 2, Quality = 0 },
+				new Item { Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7 },
+				new Item { Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80 },
+				new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 15, Quality = 20 },
+				new Item { Name = "Conjured Mana Cake", SellIn = 3, Quality = 6 },
+			};
+		}
+
+		public static readonly IList<Item> Items = CreateDayOne();
 	}
 
 	public class Item
