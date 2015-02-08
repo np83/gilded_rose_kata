946cf2fc14ae2c610298242bd8986e0c50af39ea
np83 <p_wijnhorst@hotmail.com>
2015-02-08 19:07:23 +0100

    Added log book writing; this ouput should probable not change :)

---
+++ LogBookCreation.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GildedRose.Tests
+{
+	[TestFixture]
+	public class LogBookCreation
+	{
+		[Test]
+		public void WriteLogBook()
+		{
+			var fileName = @"..\..\logbook.txt";
+			var contentBefore = System.IO.File.ReadAllText(fileName);
+			System.IO.File.Copy(fileName, @"..\..\logbook_before.txt", true);
+
+			var store = new GildedRoseStore();
+
+			var logWriter = new LogBookWriter(store, new DateTime(1812,3, 2), fileName);
+
+			logWriter.WriteHeader();
+			logWriter.InventoryReport();
+
+			for (int i = 0; i < 50; i++)
+			{
+				logWriter.AppendDayReport();
+				logWriter.InventoryReport();
+			}
+
+			var contentAfter = System.IO.File.ReadAllText(fileName);
+
+			Assert.That(contentBefore, Is.EqualTo(contentAfter));
+		}
+	}
+}
---
+++ LogBookWriter.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GildedRose.Tests
+{
+	public class LogBookWriter
+	{
+		private readonly GildedRoseStore store;
+		private DateTime today;
+		private readonly string file;
+
+		public LogBookWriter(GildedRoseStore store, DateTime today, string file)
+		{
+			this.store = store;
+			this.today = today;
+			this.file = file;
+
+			System.IO.File.WriteAllText(file, "");
+		}
+
+		public void WriteHeader()
+		{
+			var sb = new StringBuilder();
+			sb.AppendLine("This is the logbook of the GildedRose.");
+			AddRuler(sb);
+
+			WriteToDisk(sb.ToString());
+		}
+
+		public void AppendDayReport()
+		{
+			today = today.AddDays(1);
+			var sb = new StringBuilder();
+			sb.AppendLine();
+			sb.AppendLine();
+			AddRuler(sb);
+			sb.AppendLine(String.Format("Today is {0}, may it bring nothing but good.", today.ToLongDateString()));
+			WriteToDisk(sb.ToString());
+		}
+
+		public void InventoryReport()
+		{
+			var sb = new StringBuilder();
+			AddRuler(sb);
+			sb.AppendLine(String.Format("The current inventory holds {0} items:", store.Items.Count));
+			AddRuler(sb);
+			foreach (var item in store.Items)
+			{
+				sb.AppendLine(String.Format("Of quality {0}\t to be sold before {1}\t {2}", item.Quality, item.SellIn, item.Name));
+			}
+			AddRuler(sb);
+
+			WriteToDisk(sb.ToString());
+		}
+
+		private static void AddRuler(StringBuilder sb)
+		{
+			sb.AppendLine(new String('-', 50));
+		}
+
+		private void WriteToDisk(string text)
+		{
+			System.IO.File.AppendAllText(file, text);
+		}
+	}
+}
